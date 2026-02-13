ffbbf35fdf15b6d9a3e5c11ea6cf0d1746a5f4d3
tanjotsingh <tanjotsingh@localhost>
2026-02-13 15:11:48 -0500

    dependent-jobs created

---
+++ src/ice5_tanjotsingh/ICE5_TanjotSingh.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ice5_tanjotsingh;
+
+/**
+ *
+ * @author tanjotsingh
+ */
+public class ICE5_TanjotSingh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
